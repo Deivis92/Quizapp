e93656a282f9bd0ec6ea91c6c8ed92d3333d3c7a
Deividas Kondratjevas <deividaskond@gmail.com>
2024-06-03 14:24:34 +0200

    pirmas

---
+++ script.js
@@ -0,0 +1,43 @@
+let questions = [
+    {
+    "question": "Welches Land ist für seine Tulpen und Windmühlen bekannt?",
+    "answer_1": "a) Spanien",
+    "answer_2": "a) Italien",
+    "answer_3": "a) Niederlande",
+    "answer_4": "a) Frankreich",
+    "right_answer": 3
+    },
+    {
+    "question": "Welches Land ist berühmt für die Pyramiden von Gizeh?",
+    "answer_1": "a) Griechenland",
+    "answer_2": "b) Ägypten",
+    "answer_3": "c) Mexiko",
+    "answer_4": "d) Indien",
+    "right_answer": 2
+    },
+    {
+    "question": "In welcher Stadt befindet sich die berühmte Sehenswürdigkeit „Sagrada Familia“?",
+    "answer_1": "a) Paris",
+    "answer_2": "a) Madrid",
+    "answer_3": "a) Rom",
+    "answer_4": "a) Barcelona",
+    "right_answer": 4
+    },
+    {
+    "question": "Welcher Fluss fließt durch London?",
+    "answer_1": "a) Seine",
+    "answer_2": "a) Rhein",
+    "answer_3": "a) Themse",
+    "answer_4": "a) Donau",
+    "right_answer": 3
+    },
+    {
+    "question": "Wo befindet sich der Grand Canyon?",
+    "answer_1": "a) Kanada",
+    "answer_2": "b) Australien",
+    "answer_3": "c) USA",
+    "answer_4": "d) Brasilien",
+    "right_answer": 3
+    }
+
+];
